e718e6c145cf6ebec651df03e6c19a477d4411cd
Ricardo Borutta <ricardo.borutta@hotmail.com>
2024-11-19 19:27:19 +0100

    test

--- main.s
+++ main.s
@@ -2,7 +2,7 @@
 #   A simple hello world application
 #
 
-.intel_syntax noprefix
+#.intel_syntax noprefix
 .global _start
 
 .set SYS_write, 1
@@ -12,11 +12,11 @@
 .set stdout, 1
 .set stderr, 2
 
-.text
+.section .text
 _start:
     push stdout
-    lea rax, [msg]
-    push rax
+    lea msg, %rax
+    push %rax
     push msg_len
     call write
 
@@ -24,22 +24,22 @@ _start:
     call exit
 
 write:
-    mov rax, SYS_write  # Syscall id write
-    mov rdi, [rsp + 24] # File descriptor
-    mov rsi, [rsp + 16] # Buffer
-    mov rdx, [rsp + 8]  # Buffer length 
+    mov %rax, SYS_write  # Syscall id write
+    mov %rdi, 24(%rsp) # File descriptor
+    mov %rsi, 16(%rsp) # Buffer
+    mov %rdx, 8(%rsp)  # Buffer length 
     syscall
-    ret 24              # Return and clean up the stack
+    ret $24              # Return and clean up the stack
 
 exit:
-    mov rax, SYS_exit   # Syscall id exit
-    mov rdi, [rsp + 8]  # Exit code
+    mov %rax, SYS_exit   # Syscall id exit
+    mov %rdi, 8(%rsp)  # Exit code
     syscall
-    ret 8               # Return and clean up the stack
+    ret $8               # Return and clean up the stack
 
 
 .data
 msg: 
     .asciz "Hello World!\n"
 msg_len:
-    .int $ - msg
+    .int . - msg
